64eb5239397dd1c12abdf75a486ad939f67176d4
igarashi.aya <igarashi.aya@skyseer.co.jp>
2024-07-11 16:37:22 +0900

    change2

--- routers/auth.js
+++ routers/auth.js
@@ -1,65 +1,123 @@
 const router = require("express").Router();
-const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const generatedIdenticon = require("../utils/generateIdenticon");
-
-const prisma = new PrismaClient();
+const supabase = require("../lib/supabaseClient");
 
 // 新規ユーザー登録API
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
 
   const defaultIconImage = generatedIdenticon(email);
-
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // prisma.userのuserは型名の小文字
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-      profile: {
-        create: {
-          bio: "はじめまして",
-          profileImageUrl: defaultIconImage,
-        },
+  const { data: user, error } = await supabase
+    .from("User")
+    .insert([
+      {
+        username,
+        email,
+        password: hashedPassword,
+        profileImageUrl: defaultIconImage,
       },
-    },
-    include: {
-      profile: true,
-    },
-  });
-  return res.json({ user });
+    ])
+    .single();
+
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json({ user });
 });
 
 // ユーザーログインAPI
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
+
+  const { data: user, error } = await supabase
+    .from("User")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  if (error)
     return res.status(401).json({ error: "そのユーザーは存在しません。" });
-  }
 
-  const isPasswordVaild = await bcrypt.compare(password, user.password);
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
-  if (!isPasswordVaild) {
+  if (!isPasswordValid)
     return res.status(401).json({ error: "そのパスワードは間違っています。" });
-  }
 
-  // jwt.sign(ペイロード, SecretOrPrivateKey, [オプション, コールバック])
-  // (非同期) コールバックが指定された場合、コールバックはerrまたは JWT を使用して呼び出されます。
-  // (同期) JsonWebToken を文字列として返します。
-  // payload有効な JSON を表すオブジェクト リテラル、バッファ、または文字列を指定できます。
-  // SECRET_KEYは、envに隠すかランダムな10桁以上の値にすることが望ましい
-  // const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
   const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-    // クライアント側で操作するためのtokenを生成
-    expiresIn: "24h", // tokenの有効期限は1日
+    expiresIn: "24h",
   });
-  return res.json({ token });
+
+  res.json({ token });
 });
 
 module.exports = router;
+
+// const router = require("express").Router();
+// const { PrismaClient } = require("@prisma/client");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// const generatedIdenticon = require("../utils/generateIdenticon");
+
+// const prisma = new PrismaClient();
+
+// // 新規ユーザー登録API
+// router.post("/register", async (req, res) => {
+//   const { username, email, password } = req.body;
+
+//   const defaultIconImage = generatedIdenticon(email);
+
+//   const hashedPassword = await bcrypt.hash(password, 10);
+
+//   // prisma.userのuserは型名の小文字
+//   const user = await prisma.user.create({
+//     data: {
+//       username,
+//       email,
+//       password: hashedPassword,
+//       profile: {
+//         create: {
+//           bio: "はじめまして",
+//           profileImageUrl: defaultIconImage,
+//         },
+//       },
+//     },
+//     include: {
+//       profile: true,
+//     },
+//   });
+//   return res.json({ user });
+// });
+
+// // ユーザーログインAPI
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   const user = await prisma.user.findUnique({ where: { email } });
+//   if (!user) {
+//     return res.status(401).json({ error: "そのユーザーは存在しません。" });
+//   }
+
+//   const isPasswordVaild = await bcrypt.compare(password, user.password);
+
+//   if (!isPasswordVaild) {
+//     return res.status(401).json({ error: "そのパスワードは間違っています。" });
+//   }
+
+//   // jwt.sign(ペイロード, SecretOrPrivateKey, [オプション, コールバック])
+//   // (非同期) コールバックが指定された場合、コールバックはerrまたは JWT を使用して呼び出されます。
+//   // (同期) JsonWebToken を文字列として返します。
+//   // payload有効な JSON を表すオブジェクト リテラル、バッファ、または文字列を指定できます。
+//   // SECRET_KEYは、envに隠すかランダムな10桁以上の値にすることが望ましい
+//   // const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+//   const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+//     // クライアント側で操作するためのtokenを生成
+//     expiresIn: "24h", // tokenの有効期限は1日
+//   });
+//   return res.json({ token });
+// });
+
+// module.exports = router;
--- routers/posts.js
+++ routers/posts.js
@@ -1,88 +1,146 @@
 const router = require("express").Router();
-const { PrismaClient } = require("@prisma/client");
-const isAuthenticated = require("../middlewares/isAuthenticated");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const generatedIdenticon = require("../utils/generateIdenticon");
+const supabase = require("../lib/supabaseClient");
 
-const prisma = new PrismaClient();
+// 新規ユーザー登録API
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
-// つぶやき投稿用API
-router.post("/post", isAuthenticated, async (req, res) => {
-  const { content } = req.body;
+  const defaultIconImage = generatedIdenticon(email);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  if (!content) {
-    return res.status(400).json({ message: "投稿内容がありません" });
-  }
-
-  try {
-    const newPost = await prisma.post.create({
-      data: {
-        content,
-        authorId: req.userId,
+  const { data: user, error } = await supabase
+    .from("User")
+    .insert([
+      {
+        username,
+        email,
+        password: hashedPassword,
+        profileImageUrl: defaultIconImage,
       },
-      include: {
-        author: {
-          include: {
-            profile: true,
-          },
-        },
-      },
-    });
+    ])
+    .single();
 
-    res.status(201).json(newPost);
-  } catch (error) {
-    console.error("Error creating post:", error);
-    res
-      .status(500)
-      .json({ message: "サーバーエラーです。", error: error.message });
-  }
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json({ user });
 });
 
-// 最新つぶやき取得用API
-router.get("/get_latest_post", async (req, res) => {
-  try {
-    const latestPosts = await prisma.post.findMany({
-      take: 10,
-      orderBy: { createdAt: "desc" },
-      include: {
-        author: {
-          include: {
-            profile: true,
-          },
-        },
-      },
-    });
-    res.json(latestPosts);
-  } catch (error) {
-    console.error("Error fetching latest posts:", error);
-    res
-      .status(500)
-      .json({ message: "サーバーエラーです。", error: error.message });
-  }
-});
+// ユーザーログインAPI
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-// プロフィール画面で該当のユーザーの投稿内容だけを取得する
-router.get("/:userId", async (req, res) => {
-  const { userId } = req.params;
+  const { data: user, error } = await supabase
+    .from("User")
+    .select("*")
+    .eq("email", email)
+    .single();
 
-  try {
-    const userPosts = await prisma.post.findMany({
-      where: {
-        authorId: parseInt(userId),
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-      include: {
-        author: true,
-      },
-    });
-    res.status(200).json(userPosts);
-  } catch (error) {
-    console.error("Error fetching user posts:", error);
-    res
-      .status(500)
-      .json({ message: "サーバーエラーです。", error: error.message });
-  }
+  if (error)
+    return res.status(401).json({ error: "そのユーザーは存在しません。" });
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid)
+    return res.status(401).json({ error: "そのパスワードは間違っています。" });
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+
+  res.json({ token });
 });
 
 module.exports = router;
+
+// const router = require("express").Router();
+// const { PrismaClient } = require("@prisma/client");
+// const isAuthenticated = require("../middlewares/isAuthenticated");
+// require("dotenv").config();
+
+// const prisma = new PrismaClient();
+
+// // つぶやき投稿用API
+// router.post("/post", isAuthenticated, async (req, res) => {
+//   const { content } = req.body;
+
+//   if (!content) {
+//     return res.status(400).json({ message: "投稿内容がありません" });
+//   }
+
+//   try {
+//     const newPost = await prisma.post.create({
+//       data: {
+//         content,
+//         authorId: req.userId,
+//       },
+//       include: {
+//         author: {
+//           include: {
+//             profile: true,
+//           },
+//         },
+//       },
+//     });
+
+//     res.status(201).json(newPost);
+//   } catch (error) {
+//     console.error("Error creating post:", error);
+//     res
+//       .status(500)
+//       .json({ message: "サーバーエラーです。", error: error.message });
+//   }
+// });
+
+// // 最新つぶやき取得用API
+// router.get("/get_latest_post", async (req, res) => {
+//   try {
+//     const latestPosts = await prisma.post.findMany({
+//       take: 10,
+//       orderBy: { createdAt: "desc" },
+//       include: {
+//         author: {
+//           include: {
+//             profile: true,
+//           },
+//         },
+//       },
+//     });
+//     res.json(latestPosts);
+//   } catch (error) {
+//     console.error("Error fetching latest posts:", error);
+//     res
+//       .status(500)
+//       .json({ message: "サーバーエラーです。", error: error.message });
+//   }
+// });
+
+// // プロフィール画面で該当のユーザーの投稿内容だけを取得する
+// router.get("/:userId", async (req, res) => {
+//   const { userId } = req.params;
+
+//   try {
+//     const userPosts = await prisma.post.findMany({
+//       where: {
+//         authorId: parseInt(userId),
+//       },
+//       orderBy: {
+//         createdAt: "desc",
+//       },
+//       include: {
+//         author: true,
+//       },
+//     });
+//     res.status(200).json(userPosts);
+//   } catch (error) {
+//     console.error("Error fetching user posts:", error);
+//     res
+//       .status(500)
+//       .json({ message: "サーバーエラーです。", error: error.message });
+//   }
+// });
+
+// module.exports = router;
