6d293942d15614e84eee64d93b02d7f395ee0353
igarashi.aya <igarashi.aya@skyseer.co.jp>
2024-07-11 11:58:14 +0900

    CORS

--- server.js
+++ server.js
@@ -1,18 +1,57 @@
 const express = require("express");
-const app = express();
-const authRoute = require("./routers/auth");
-const postsRoute = require("./routers/posts");
-const usersRoute = require("./routers/users");
 const cors = require("cors");
+require("dotenv").config();
 
-// macの場合5000だとコンフリクトを起こす場合がある
-const PORT = 5080;
+const app = express();
 
-app.use(cors());
-// expressでjson形式を使う設定をする
-app.use(express.json()); // body-parserの役割を兼ねている
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postsRoute);
-app.use("/api/users", usersRoute);
+// CORSの設定
+const allowedOrigins = [
+  "https://udemy-sns-client-bdefxfw5t-aya-igarashis-projects.vercel.app",
+];
 
-app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // 受け入れるオリジンを許可
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json()); // JSONパーシングのためのミドルウェア
+
+// ルートとその他の設定
+app.use("/api/auth", require("./api/routers/auth"));
+app.use("/api/posts", require("./api/routers/posts"));
+app.use("/api/users", require("./api/routers/users"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// const express = require("express");
+// const app = express();
+// const authRoute = require("./routers/auth");
+// const postsRoute = require("./routers/posts");
+// const usersRoute = require("./routers/users");
+// const cors = require("cors");
+
+// // macの場合5000だとコンフリクトを起こす場合がある
+// const PORT = 5080;
+
+// app.use(cors());
+// // expressでjson形式を使う設定をする
+// app.use(express.json()); // body-parserの役割を兼ねている
+// app.use("/api/auth", authRoute);
+// app.use("/api/posts", postsRoute);
+// app.use("/api/users", usersRoute);
+
+// app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
